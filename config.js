1076fb5c539d097e03831a3a6badf93819324b18
Sally-A <Sally.Anderson@gmx.com>
2021-04-27 21:06:11 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '907989683383033',
+        clientSecret: '110fa531aa24f70133961c928bedc350'
+    }
 }
